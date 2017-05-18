dd66dd28b0c81a7abd148cdb31e435c9f6a291c8
Kong Le <Kong Le>
2017-05-18 16:01:26 -0400

    handlebars and scrape working

--- server.js
+++ server.js
@@ -4,14 +4,40 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     logger = require("morgan"),
     mongoose = require("mongoose"),
-    Note = require("./models/Note.js"),
-    Article = require("./models/Article.js"),
+    comments = require("./models/comments.js"),
+    articles = require("./models/articles.js"),
     request = require("request"),
     cheerio = require("cheerio"),
     exphbs = require("express-handlebars"),
     app = express(),
     port = 3000;
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
 mongoose.Promise = Promise;
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
+var url = "http://www.muscleandfitness.com/athletes-celebrities/news";
+app.get("/", function (req, res) {
+res.render("index");
+});
+app.get("/scrape", function (req, res) {
+    request(url, function (error, response, html) {
+        var result = [];
+        var $ = cheerio.load(html);
+        $(".node__title").each(function (i, element) {
+            var title = $(this).children().text().trim(),
+                link = "http://www.muscleandfitness.com" + $(this).children().attr("href");
+            result.push({
+                title: title,
+                link: link
+            });
+        });
+        res.send(result);
+    });
+});
+
+
+app.listen(port, function () {
+    console.log("App running on port " + port);
+});
